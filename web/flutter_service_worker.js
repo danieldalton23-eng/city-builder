6a204b13e93e652911ec46dc14138a52848730a1
danieldalton23-eng <danieldalton23-eng@users.noreply.github.com>
2026-03-27 19:17:19 -0400

    Deploy: UI redesign v6 (correct build)

--- web/flutter_service_worker.js
+++ web/flutter_service_worker.js
@@ -14,7 +14,7 @@ const RESOURCES = {"canvaskit/skwasm.worker.js": "89990e8c92bcb123999aa81f7e203b
 "canvaskit/canvaskit.js.symbols": "efc2cd87d1ff6c586b7d4c7083063a40",
 "canvaskit/skwasm.js": "ac0f73826b925320a1e9b0d3fd7da61c",
 "flutter.js": "4b2350e14c6650ba82871f60906437ea",
-"flutter_bootstrap.js": "65819e9ab1e5471435f31ed464c5226c",
+"flutter_bootstrap.js": "96fbf74cf83fc45134bdf289e8b8d2f7",
 "index.html": "798c8de6ab902520297dd63e3f646ade",
 "/": "798c8de6ab902520297dd63e3f646ade",
 "main.dart.js": "2f161f3578af3facdfbe3b79b1392f96",
@@ -27,11 +27,11 @@ const RESOURCES = {"canvaskit/skwasm.worker.js": "89990e8c92bcb123999aa81f7e203b
 "assets/AssetManifest.bin.json": "69a99f98c8b1fb8111c5fb961769fcd8",
 "assets/FontManifest.json": "dc3d03800ccca4601324923c0b1d6d57",
 "assets/NOTICES": "edfcb945c371ee6e6565197c13cf8b82",
-"favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
+"favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "manifest.json": "1e2e883910dd187f0bdc6b07b12122c0"};
 // The application shell files that are downloaded before a service worker can
 // start.
