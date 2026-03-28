38637c24f7f668485c8c8cbf2c74d4ffea14cc99
danieldalton23-eng <danieldalton23-eng@users.noreply.github.com>
2026-03-27 20:05:12 -0400

    Deploy: isometric 3D redesign - My Colony style buildings and grid

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -15,8 +15,8 @@ const RESOURCES = {"canvaskit/skwasm.worker.js": "89990e8c92bcb123999aa81f7e203b
 "canvaskit/skwasm.js": "ac0f73826b925320a1e9b0d3fd7da61c",
 "flutter.js": "4b2350e14c6650ba82871f60906437ea",
 "flutter_bootstrap.js": "cb44d70435d57368b4e3d765b046d199",
-"index.html": "ebcfee9552278f7a75e5771dc3c6dd3b",
-"/": "ebcfee9552278f7a75e5771dc3c6dd3b",
+"index.html": "798c8de6ab902520297dd63e3f646ade",
+"/": "798c8de6ab902520297dd63e3f646ade",
 "main.dart.js": "2f161f3578af3facdfbe3b79b1392f96",
 "version.json": "4ad6576f78ed087f3b04ad62dddd3b31",
 "assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "e986ebe42ef785b27164c36a9abc7818",
@@ -27,11 +27,11 @@ const RESOURCES = {"canvaskit/skwasm.worker.js": "89990e8c92bcb123999aa81f7e203b
 "assets/AssetManifest.bin.json": "69a99f98c8b1fb8111c5fb961769fcd8",
 "assets/FontManifest.json": "dc3d03800ccca4601324923c0b1d6d57",
 "assets/NOTICES": "edfcb945c371ee6e6565197c13cf8b82",
+"favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "manifest.json": "1e2e883910dd187f0bdc6b07b12122c0"};
 // The application shell files that are downloaded before a service worker can
 // start.
